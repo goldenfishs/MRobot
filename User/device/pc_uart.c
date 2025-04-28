5cbcadbb31207d6d189d0a5fcc801c8b4fffa28d
RB <robofish>
2025-04-28 20:25:26 +0800

    添加修改

--- User/device/pc_uart.c
+++ User/device/pc_uart.c
@@ -6,6 +6,8 @@
 #include "bsp\uart.h"
 #include "device.h"
 
+#define UART_HANDLE BSP_UART_GetHandle(BSP_UART_PC)
+
 #define AI_LEN_RX_BUFF (sizeof(UART_RxData_t))
 
 static bool rx_flag = false;
@@ -17,14 +19,15 @@ static void UART_RxCpltCallback(void) { rx_flag = true; }
 int UART_Init(UART_t *huart)
 {
     UNUSED(huart);
-    HAL_UART_Receive_DMA(&BSP_UART_PC, rxbuf, AI_LEN_RX_BUFF);
+    //注册回调函数
+    HAL_UART_RegisterCallback(UART_HANDLE, BSP_UART_RX_CPLT_CB, UART_RxCpltCallback);
     return DEVICE_OK
 }
 
 int UART_StartReceive(UART_t *huart)
 {
     UNUSED(huart);
-    HAL_UART_Receive_DMA(&BSP_UART_PC, rxbuf, AI_LEN_RX_BUFF);
+    HAL_UART_Receive_DMA(UART_HANDLE, rxbuf, AI_LEN_RX_BUFF);
     return DEVICE_OK;
 }
 
@@ -46,7 +49,7 @@ void UART_PackTx(UART_t *huart, UART_TxData_t *tx_data)
 
 int8_t UART_StartSend(UART_t *huart)
 {
-    if (HAL_UART_Transmit_DMA(&BSP_UART_PC, huart->tx_data, sizeof(UART_TxData_t)) == HAL_OK)
+    if (HAL_UART_Transmit_DMA(UART_HANDLE, huart->tx_data, sizeof(UART_TxData_t)) == HAL_OK)
     {
         return DEVICE_OK
     }
